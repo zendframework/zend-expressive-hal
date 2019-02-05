f4e120a659bd146ea4dfe1d6a99f6c703d9672c9
Matthew Weier O'Phinney <matthew@zend.com>
2019-02-05 14:53:40 -0600

    refactor: extract common pagination routines
    Extracts two methods, `createPaginatedCollectionResource()` and
    `createCollectionResource()` from the bodies of each of
    `extractPaginator()` and `extractDoctrinePaginator()`. The first method
    determines if we have a known pagination type; if not, it simply
    generates a self relational link and creates a HAL collection by
    iterating the paginator. If it does know the pagination type, it will
    generate any necessary pagination links before iterating the collection
    to produce the resource.
    
    These changes simplify the logic of `extractPaginator()` and
    `extractDoctrinePaginator()`, allowing them to focus primarily on how
    they determine the required pagination information.

--- src/ResourceGenerator/ExtractCollectionTrait.php
+++ src/ResourceGenerator/ExtractCollectionTrait.php
@@ -79,56 +79,20 @@ trait ExtractCollectionTrait
         ResourceGenerator $resourceGenerator,
         ServerRequestInterface $request
     ) : HalResource {
-        $data  = ['_total_items' => $collection->getTotalItemCount()];
-        $links = [];
+        $data      = ['_total_items' => $collection->getTotalItemCount()];
+        $pageCount = $collection->count();
 
-        $paginationParamType = $metadata->getPaginationParamType();
-        if (in_array($paginationParamType, $this->paginationTypes, true)) {
-            // Supports pagination
-            $pageCount = $collection->count();
-
-            $paginationParam = $metadata->getPaginationParam();
-            $page = $paginationParamType === AbstractCollectionMetadata::TYPE_QUERY
-                ? (int) ($request->getQueryParams()[$paginationParam] ?? 1)
-                : (int) $request->getAttribute($paginationParam, 1);
-
-            if ($page < 1 || ($page > $pageCount && $pageCount > 0)) {
-                throw new Exception\OutOfBoundsException(sprintf(
-                    'Page %d is out of bounds. Collection has %d page%s.',
-                    $page,
-                    $pageCount,
-                    $pageCount > 1 ? 's' : ''
-                ));
-            }
-
-            $collection->setCurrentPageNumber($page);
-
-            $links[] = $this->generateLinkForPage('self', $page, $metadata, $resourceGenerator, $request);
-            if ($page > 1) {
-                $links[] = $this->generateLinkForPage('first', 1, $metadata, $resourceGenerator, $request);
-                $links[] = $this->generateLinkForPage('prev', $page - 1, $metadata, $resourceGenerator, $request);
-            }
-            if ($page < $pageCount) {
-                $links[] = $this->generateLinkForPage('next', $page + 1, $metadata, $resourceGenerator, $request);
-                $links[] = $this->generateLinkForPage('last', $pageCount, $metadata, $resourceGenerator, $request);
-            }
-
-            $data['_page'] = $page;
-            $data['_page_count'] = $pageCount;
-        }
-
-        if (empty($links)) {
-            $links[] = $this->generateSelfLink($metadata, $resourceGenerator, $request);
-        }
-
-        $resources = [];
-        foreach ($collection as $item) {
-            $resources[] = $resourceGenerator->fromObject($item, $request);
-        }
-
-        return new HalResource($data, $links, [
-            $metadata->getCollectionRelation() => $resources,
-        ]);
+        return $this->createPaginatedCollectionResource(
+            $pageCount,
+            $data,
+            function (int $page) use ($collection) {
+                $collection->setCurrentPageNumber($page);
+            },
+            $collection,
+            $metadata,
+            $resourceGenerator,
+            $request
+        );
     }
 
     /**
@@ -150,52 +114,18 @@ trait ExtractCollectionTrait
         $pageCount  = (int) ceil($totalItems / $perPage);
 
         $data  = ['_total_items' => $totalItems];
-        $links = [];
 
-        $paginationParamType = $metadata->getPaginationParamType();
-        if (in_array($paginationParamType, $this->paginationTypes, true)) {
-            $paginationParam = $metadata->getPaginationParam();
-            $page = $paginationParamType === AbstractCollectionMetadata::TYPE_QUERY
-                ? (int) ($request->getQueryParams()[$paginationParam] ?? 1)
-                : (int) $request->getAttribute($paginationParam, 1);
-
-            if ($page < 1 || ($page > $pageCount && $pageCount > 0)) {
-                throw new Exception\OutOfBoundsException(sprintf(
-                    'Page %d is out of bounds. Collection has %d page%s.',
-                    $page,
-                    $pageCount,
-                    $pageCount > 1 ? 's' : ''
-                ));
-            }
-
-            $query->setFirstResult($pageCount * ($page - 1));
-
-            $links[] = $this->generateLinkForPage('self', $page, $metadata, $resourceGenerator, $request);
-            if ($page > 1) {
-                $links[] = $this->generateLinkForPage('first', 1, $metadata, $resourceGenerator, $request);
-                $links[] = $this->generateLinkForPage('prev', $page - 1, $metadata, $resourceGenerator, $request);
-            }
-            if ($page < $pageCount) {
-                $links[] = $this->generateLinkForPage('next', $page + 1, $metadata, $resourceGenerator, $request);
-                $links[] = $this->generateLinkForPage('last', $pageCount, $metadata, $resourceGenerator, $request);
-            }
-
-            $data['_page'] = $page;
-            $data['_page_count'] = $pageCount;
-        }
-
-        if (empty($links)) {
-            $links[] = $this->generateSelfLink($metadata, $resourceGenerator, $request);
-        }
-
-        $resources = [];
-        foreach ($collection as $item) {
-            $resources[] = $resourceGenerator->fromObject($item, $request);
-        }
-
-        return new HalResource($data, $links, [
-            $metadata->getCollectionRelation() => $resources,
-        ]);
+        return $this->createPaginatedCollectionResource(
+            $pageCount,
+            $data,
+            function (int $page) use ($query, $pageCount) {
+                $query->setFirstResult($pageCount * ($page - 1));
+            },
+            $collection,
+            $metadata,
+            $resourceGenerator,
+            $request
+        );
     }
 
     private function extractIterator(
@@ -219,6 +149,118 @@ trait ExtractCollectionTrait
             $resourceGenerator,
             $request
         )];
+
+        return new HalResource($data, $links, [
+            $metadata->getCollectionRelation() => $resources,
+        ]);
+    }
+
+    /**
+     * Create a collection resource representing a paginated set.
+     *
+     * Determines if the metadata uses a query or placeholder pagination type.
+     * If not, it generates a self relational link, and then immediately creates
+     * and returns a collection resource containing every item in the collection.
+     *
+     * If it does, it pulls the pagination parameter from the request using the
+     * appropriate source (query string arguments or routing parameter), and
+     * then checks to see if we have a valid page number, throwing an out of
+     * bounds exception if we do not. From the page, it then determines which
+     * relational pagination links to create, including a `self` relation,
+     * and aggregates the current page and total page count in the $data array
+     * before calling on createCollectionResource() to generate the final
+     * HAL resource instance.
+     *
+     * @param array<string, mixed> $data Data to render in the root of the HAL
+     *     resource.
+     * @param callable $notifyCollectionOfPage A callback that receives an integer
+     *     $page argument; this should be used to update the paginator instance
+     *     with the current page number.
+     */
+    private function createPaginatedCollectionResource(
+        int $pageCount,
+        array $data,
+        callable $notifyCollectionOfPage,
+        iterable $collection,
+        AbstractCollectionMetadata $metadata,
+        ResourceGenerator $resourceGenerator,
+        ServerRequestInterface $request
+    ) : HalResource {
+        $links               = [];
+        $paginationParamType = $metadata->getPaginationParamType();
+
+        if (! in_array($paginationParamType, $this->paginationTypes, true)) {
+            $links[] = $this->generateSelfLink($metadata, $resourceGenerator, $request);
+            return $this->createCollectionResource(
+                $links,
+                $data,
+                $collection,
+                $metadata,
+                $resourceGenerator,
+                $request
+            );
+        }
+
+        $paginationParam = $metadata->getPaginationParam();
+        $page = $paginationParamType === AbstractCollectionMetadata::TYPE_QUERY
+            ? (int) ($request->getQueryParams()[$paginationParam] ?? 1)
+            : (int) $request->getAttribute($paginationParam, 1);
+
+        if ($page < 1 || ($page > $pageCount && $pageCount > 0)) {
+            throw new Exception\OutOfBoundsException(sprintf(
+                'Page %d is out of bounds. Collection has %d page%s.',
+                $page,
+                $pageCount,
+                $pageCount > 1 ? 's' : ''
+            ));
+        }
+
+        $notifyCollectionOfPage($page);
+
+        $links[] = $this->generateLinkForPage('self', $page, $metadata, $resourceGenerator, $request);
+        if ($page > 1) {
+            $links[] = $this->generateLinkForPage('first', 1, $metadata, $resourceGenerator, $request);
+            $links[] = $this->generateLinkForPage('prev', $page - 1, $metadata, $resourceGenerator, $request);
+        }
+        if ($page < $pageCount) {
+            $links[] = $this->generateLinkForPage('next', $page + 1, $metadata, $resourceGenerator, $request);
+            $links[] = $this->generateLinkForPage('last', $pageCount, $metadata, $resourceGenerator, $request);
+        }
+
+        $data['_page'] = $page;
+        $data['_page_count'] = $pageCount;
+
+        return $this->createCollectionResource(
+            $links,
+            $data,
+            $collection,
+            $metadata,
+            $resourceGenerator,
+            $request
+        );
+    }
+
+    /**
+     * Create the collection resource with its embedded resources.
+     *
+     * Iterates the collection, passing each item to the resource generator
+     * to produce a HAL resource. These are then used to create an embedded
+     * relation in a master HAL resource that contains metadata around the
+     * collection itself (number of items, number of pages, etc.), and any
+     * relational links.
+     */
+    private function createCollectionResource(
+        array $links,
+        array $data,
+        iterable $collection,
+        AbstractCollectionMetadata $metadata,
+        ResourceGenerator $resourceGenerator,
+        ServerRequestInterface $request
+    ) : HalResource {
+        $resources = [];
+        foreach ($collection as $item) {
+            $resources[] = $resourceGenerator->fromObject($item, $request);
+        }
 
         return new HalResource($data, $links, [
             $metadata->getCollectionRelation() => $resources,
